3f949997c3f1522c833940e84bf6e3f717f7b412
Leslie Mwangi <luciannahphoi@gmail.com>
2024-05-02 11:56:51 +0300

    labs

--- src/components/About.js
+++ src/components/About.js
@@ -2,17 +2,22 @@ import React from "react";
 import Links from "./Links";
 
 function About(props) {
+  if (!props.bio) {
+    return null;
+  } else if (props.bio.trim()==="") {
+    return null;
+  }else{
+  
+  
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <Links
-        github="https://github.com/liza"
-        linkedin="https://www.linkedin.com/in/liza/"
-      />
+      <Links github="https://github.com/liza" linkedin= "https://www.linkedin.com/in/liza/"/>
     </div>
   );
+  }
 }
 
 export default About;
